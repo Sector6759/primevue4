bc0a6755f55f0e2b743b68215093549f063c4391
Sector6759 <149817326+Sector6759@users.noreply.github.com>
2024-07-21 18:09:24 +0200

    fix: properly register components

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,11 @@
+import Lara from "@primevue/themes/lara";
+import Button from "primevue/button";
+import PrimeVue from "primevue/config";
 import { createApp } from "vue";
 import App from "./App.vue";
 
-createApp(App).mount("#app");
+const app = createApp(App);
+app.use(PrimeVue, { theme: { preset: Lara } });
+app.component("Button", Button);
+
+app.mount("#app");
